bf158db372fd3b7addd60ecd6e38105062ea7c0a
nzzima <nikita-krylov-2012@mail.ru>
2025-01-29 17:07:27 +0300

    Added authorization screen

---
+++ SecretMessanger/Helpers/Extensions/UIViewController/UIViewController.ext.swift
@@ -0,0 +1,8 @@
+//
+//  UIViewController.ext.swift
+//  SecretMessanger
+//
+//  Created by Nikita Krylov on 29.01.2025.
+//
+
+import Foundation
---
+++ SecretMessanger/Module/Authentication/AuthorizationView/Model/DataModel/UserInfo.swift
@@ -0,0 +1,8 @@
+//
+//  UserInfo.swift
+//  SecretMessanger
+//
+//  Created by Nikita Krylov on 29.01.2025.
+//
+
+import Foundation
